acf65810af9267c798b5e0c59a605c69a15d0008
ytr.ugur@gmail.com <ytr.ugur@gmail.com>
2022-11-26 14:07:17 +0100

    Project kick-off

---
+++ src/main/java/yeter/ugur/insuranceexample/InsuranceExampleApplication.java
@@ -0,0 +1,13 @@
+package yeter.ugur.insuranceexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeter/ugur/insuranceexample/InsuranceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package yeter.ugur.insuranceexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
