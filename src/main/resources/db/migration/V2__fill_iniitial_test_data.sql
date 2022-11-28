e4b24d560b04aa394d57fcea517f08d8ea24acae
ytr.ugur@gmail.com <ytr.ugur@gmail.com>
2022-11-28 12:00:27 +0100

    Migrate Flyway

---
+++ src/main/resources/db/migration/V2__fill_iniitial_test_data.sql
@@ -0,0 +1,22 @@
+    INSERT INTO
+        policy
+        (created_at, external_id, start_date)
+    VALUES
+        (1669632320298, 'CU423DF89', '2022-11-29');
+
+    INSERT INTO
+        insured_person
+        (first_name, second_name, premium)
+    VALUES
+        ('Jane', 'Johnson', 12.90),
+        ('Jack', 'Doe', 15.90),
+        ('Tim', 'Button', 10);
+
+    INSERT INTO
+        policy_person
+        (policy_internal_id, person_id)
+    values
+        (1, 1),
+        (1, 2),
+        (1, 3);
+
